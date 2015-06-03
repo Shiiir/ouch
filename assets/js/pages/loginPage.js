a265047927ed3646132d7b20c0a60fcc64debd8e
malikid <malikid.chang@gmail.com>
2015-06-03 22:21:25 +0800

    Refactored LoginPage.

--- assets/js/pages/loginPage.js
+++ assets/js/pages/loginPage.js
@@ -1,9 +1,8 @@
 var LoginPage = React.createClass({
   render: function() {
-    console.log("LOGIN_TITLE", LOGIN_PAGE.TITLE);
     return (
       <div>
-        <LoginTitle data={LOGIN_PAGE.TITLE} />
+        <LoginTitle />
         <LoginForm />
       </div>
     );
@@ -14,7 +13,7 @@ var LoginTitle = React.createClass({
   render: function() {
     return (
       <div>
-        {this.props.data}
+        {LOGIN_PAGE.TITLE}
       </div>
     );
   }
@@ -22,27 +21,47 @@ var LoginTitle = React.createClass({
 
 // Reference: https://facebook.github.io/react/docs/tutorial.html#adding-new-comments
 var LoginForm = React.createClass({
+
   handleSubmit: function(e) {
+
     e.preventDefault();
+
     var usernameInput = React.findDOMNode(this.refs.username);
     var passwordInput = React.findDOMNode(this.refs.password);
     var username = usernameInput.value.trim();
     var password = passwordInput.value.trim();
-    console.log("username", username, "password", password);
+
     if (!username || !password) {
       return;
     }
-    // TODO: send request to the server
+
+    // TODO: submit to the server
+    // $.ajax({
+    //   url: this.props.url,
+    //   dataType: 'json',
+    //   type: 'POST',
+    //   data: comment,
+    //   success: function(data) {
+    //     this.setState({data: data});
+    //   }.bind(this),
+    //   error: function(xhr, status, err) {
+    //     console.error(this.props.url, status, err.toString());
+    //   }.bind(this)
+    // });
+    window.location = "index.html"
+
     usernameInput.value = '';
     passwordInput.value = '';
+
     return;
   },
+
   render: function() {
     return (
       <form onSubmit={this.handleSubmit}>
         <Input placeholder={LOGIN_PAGE.USERNAME_INPUT.PLACEHOLDER} ref={LOGIN_PAGE.USERNAME_INPUT.REF} />
         <br/>
-        <Input placeholder={LOGIN_PAGE.PASSWORD_INPUT.PLACEHOLDER} ref={LOGIN_PAGE.PASSWORD_INPUT.REF} />
+        <Input type="password" placeholder={LOGIN_PAGE.PASSWORD_INPUT.PLACEHOLDER} ref={LOGIN_PAGE.PASSWORD_INPUT.REF} />
         <br/>
         <Button type="submit" value={LOGIN_PAGE.SUBMIT_BUTTON} />
       </form>
@@ -51,20 +70,29 @@ var LoginForm = React.createClass({
 });
 
 var Input = React.createClass({
+
   render: function() {
+
+    var inputType = "text";
+
+    if(this.props.type)
+      inputType = this.props.type;
+
     return (
-      <input type="text" placeholder={this.props.placeholder} ref={this.props.ref} />
+      <input type={this.props.type} placeholder={this.props.placeholder} ref={this.props.ref} />
     );
   }
 });
 
 var Button = React.createClass({
+
   render: function() {
-    var buttonType = "";
+
+    var buttonType = "button";
+
     if(this.props.type === "submit")
       buttonType = "submit";
-    else
-      buttonType = "button";
+
     return (
       <input type={buttonType} value={this.props.value} />
     );
