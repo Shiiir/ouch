07e9d2ab38adf3cd4dcc17b6bf40a3b53cb7fe03
esbb48 <yanru_li@hiiir.com>
2015-06-14 03:13:30 +0800

    finish static component star

--- assets/js/components/star.js
+++ assets/js/components/star.js
@@ -1,1 +1,23 @@
-// React...
+var Star = React.createClass({
+  getInitialState: function() {
+    return {isActive: false};
+  },
+
+  changeStarState: function() {
+    this.setState({isActive: !this.state.isActive});
+  },
+
+  render: function() {
+    var active = "";
+    if (this.state.isActive) {
+      active = " active";
+    }
+
+    classString = "star" + active
+    return (
+      <span className={classString} onClick={this.changeStarState} >
+        ★
+      </span>
+    );
+  }
+});
